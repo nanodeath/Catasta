f9595c518d52c020518cdf9f8149d53acef38960
Max Aller <nanodeath@gmail.com>
2012-11-19 08:38:37 -0800

    Improve formatting of transform with more newlines and Ruby 1.9-style hashes.

--- lib/catasta/core/parslet_parser.rb
+++ lib/catasta/core/parslet_parser.rb
@@ -226,18 +226,73 @@ class Root < Struct.new(:nodes)
 end
 
 class CatastaRubyTransform < Parslet::Transform
-  rule(:expression => { :ruby => simple(:ruby) }) { Code.new(VariableLookup.new(ruby)) }
-  rule(:comment => { :ruby => simple(:ruby) }) { '' }
-  rule(:loop => { :i => simple(:i), :collection => simple(:collection) }, :content => sequence(:nodes)) { Loop.new(i, VariableLookup.new(collection), nodes) }
-  rule(:loop_map => { :loop_key => simple(:loop_key), :loop_value => simple(:loop_value), :collection => simple(:collection) }, :content => sequence(:nodes)) {
+  rule(
+    expression: { 
+      ruby: simple(:ruby) 
+    }
+  ) {
+    Code.new(VariableLookup.new(ruby))
+  }
+  
+  rule(
+    comment: { 
+      ruby: simple(:ruby) 
+    }
+  ) { 
+    ''
+  }
+  
+  rule(
+    loop: {
+      i: simple(:i),
+      collection: simple(:collection)
+    },
+    content: sequence(:nodes)
+  ) {
+    Loop.new(i, VariableLookup.new(collection), nodes)
+  }
+
+  rule(
+    loop_map: {
+      loop_key: simple(:loop_key),
+      loop_value: simple(:loop_value),
+      collection: simple(:collection)
+    },
+    content: sequence(:nodes)
+  ) {
     LoopMap.new(loop_key, loop_value, VariableLookup.new(collection), nodes)
   }
-  rule(:condition => { :variable => simple(:variable), :inverted => simple(:inverted) }, :content => sequence(:nodes)) { Conditional.new(inverted, VariableLookup.new(variable), nodes) }
   
-  rule(:text => simple(:text)) { Text.new(text) }
-  rule(:text => sequence(:texts)) { TextList.new(texts) }
-  rule(:content => sequence(:nodes)) { Content.new(nodes) }
-  rule(:root => subtree(:nodes)) { Root.new(nodes) }
+  rule(
+    condition: {
+      variable: simple(:variable),
+      inverted: simple(:inverted)
+    },
+    content: sequence(:nodes)
+  ) {
+    Conditional.new(inverted, VariableLookup.new(variable), nodes)
+  }
+  
+  rule(
+    text: simple(:text)
+  ) { 
+    Text.new(text)
+  }
+  rule(
+    text: sequence(:texts)
+  ) {
+    TextList.new(texts)
+  }
+  rule(
+    content: sequence(:nodes)
+  ) {
+    Content.new(nodes)
+  }
+  rule(
+    root: subtree(:nodes)
+  ) {
+    Root.new(nodes)
+  }
 end
 
 # puts "\n\nTransforms:\n"
